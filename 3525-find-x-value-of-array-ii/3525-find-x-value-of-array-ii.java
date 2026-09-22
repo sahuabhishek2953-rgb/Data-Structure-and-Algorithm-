7fbcaf71610aab8ef2d7a428765b4f54c1bc1b60
Abhishek Sahu <sahuabhishek2953@gmail.com>
2026-09-22 22:58:11 +0530

    Time: 234 ms (73.68%), Space: 281.6 MB (42.1%) - LeetHub

---
+++ 3525-find-x-value-of-array-ii/3525-find-x-value-of-array-ii.java
@@ -0,0 +1,119 @@
+class Solution {
+    static class Node {
+        int prod;
+        int[] cnt;
+
+        Node(int k) {
+            prod = 1 % k;
+            cnt = new int[k];
+        }
+    }
+
+    int k;
+    Node[] tree;
+
+    Node merge(Node a, Node b) {
+        Node res = new Node(k);
+
+        res.prod = (a.prod * b.prod) % k;
+
+        for (int i = 0; i < k; i++) {
+            res.cnt[i] = a.cnt[i];
+        }
+
+        for (int i = 0; i < k; i++) {
+            res.cnt[(a.prod * i) % k] += b.cnt[i];
+        }
+
+        return res;
+    }
+
+    void build(int node, int l, int r, int[] nums) {
+        if (l == r) {
+            tree[node].prod = nums[l] % k;
+            tree[node].cnt[nums[l] % k] = 1;
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid, nums);
+        build(node * 2 + 1, mid + 1, r, nums);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    void update(int node, int l, int r, int idx, int val) {
+        if (l == r) {
+            tree[node] = new Node(k);
+            tree[node].prod = val % k;
+            tree[node].cnt[val % k] = 1;
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (idx <= mid) {
+            update(node * 2, l, mid, idx, val);
+        } else {
+            update(node * 2 + 1, mid + 1, r, idx, val);
+        }
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    Node query(int node, int l, int r, int ql, int qr) {
+        if (ql <= l && r <= qr) {
+            return tree[node];
+        }
+
+        int mid = (l + r) / 2;
+
+        if (qr <= mid) {
+            return query(node * 2, l, mid, ql, qr);
+        }
+
+        if (ql > mid) {
+            return query(node * 2 + 1, mid + 1, r, ql, qr);
+        }
+
+        Node left = query(node * 2, l, mid, ql, qr);
+        Node right = query(node * 2 + 1, mid + 1, r, ql, qr);
+
+        return merge(left, right);
+    }
+
+    public int[] resultArray(int[] nums, int k, int[][] queries) {
+        this.k = k;
+
+        int n = nums.length;
+        tree = new Node[4 * n];
+
+        for (int i = 0; i < tree.length; i++) {
+            tree[i] = new Node(k);
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] %= k;
+        }
+
+        build(1, 0, n - 1, nums);
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int index = queries[i][0];
+            int value = queries[i][1] % k;
+            int start = queries[i][2];
+            int x = queries[i][3];
+
+            update(1, 0, n - 1, index, value);
+
+            Node res = query(1, 0, n - 1, start, n - 1);
+
+            ans[i] = res.cnt[x];
+        }
+
+        return ans;
+    }
+}
